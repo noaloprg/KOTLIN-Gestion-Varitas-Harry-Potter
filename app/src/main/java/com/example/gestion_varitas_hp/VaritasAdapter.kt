88d02a86eb29195f5415d31d82e4046123de8972
Noa López <noaloprg@gmail.com>
2026-01-11 20:09:55 +0100

    vistas terminadas y ArrayAdapter personalizado creado

---
+++ app/src/main/java/com/example/gestion_varitas_hp/VaritasAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.gestion_varitas_hp
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+
+class VaritasAdapter(context: Context, private val varitas: Array<InformacionVarita>) :
+    ArrayAdapter<InformacionVarita>(context, R.layout.item_varita, varitas) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        //crear inflador de vistas
+        val inflador = LayoutInflater.from(context)
+        //obtener el XML qeu contiene como se van a proyectar los datos
+        val vista = inflador.inflate(R.layout.item_varita, parent)
+
+        //controladores de la vista para asignar datos
+        val titulo = vista.findViewById<TextView>(R.id.tvInfoVarita)
+        val subtitulo = vista.findViewById<TextView>(R.id.tvInfoPersonaje)
+
+        //asignacion de valores a los controladores
+        titulo.text = String.format("%s - %s ", varitas[position].madera, varitas[position].nucleo)
+        subtitulo.text = varitas[position].personaje
+
+        //devuelve la vista con los datos asignados
+        return vista
+    }
+}
